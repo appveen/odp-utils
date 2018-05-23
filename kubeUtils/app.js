7fcb46649f5b94be4cc1488f14cd7b2192940281
Jerry M <developer.jerrymannel@gmail.com>
2018-05-23 12:28:38 +0530

    added connection check of kubernetes

--- kubeUtils/app.js
+++ kubeUtils/app.js
@@ -1,10 +1,13 @@
 "use strict";
 
+const req = require("./requestHandler");
+
 let e = {};
 
 e.namespace = require("./namespace")
 e.deployment = require("./deployment")
 e.service = require("./service")
 e.docker = require("./docker");
+e.check = () => req.get("/apis");
 
 module.exports = e;
