6d05af32abaefa9533dc3e666bf20d31eb6d1020
Jerry M <developer.jerrymannel@gmail.com>
2018-05-29 12:34:24 +0530

    added log4js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 let e = {};
+
+var log4js = require("log4js");
+log4js.configure({
+    levels: {
+      OFF: { value: Number.MAX_VALUE-1, colour: 'white' },
+      AUDIT: { value: Number.MAX_VALUE, colour: 'yellow' }
+    },
+    appenders: { out: { type: 'stdout' } },
+    categories: { default: { appenders: ['out'], level: 'AUDIT' } }
+  });
+log4js.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL: 'info';
+
+const logger = global.logger;
+
 e.auditTrail = require('./auditTrail');
 e.kubeutil = require("./kubeUtils/app");
 
