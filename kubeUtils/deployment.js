a65cae9d39c178fb59ffd4dda77bca5fa8df6df5
Shobhit Agarwal <shobhit@capiot.com>
2018-07-23 11:54:42 +0530

    docker sercet added

--- kubeUtils/deployment.js
+++ kubeUtils/deployment.js
@@ -8,43 +8,43 @@ var e = {};
 
 e.getAllDeployments = () => {
 	return req.get(_baseURL + "/deployments")
-	.then(_d => {
-		var data = _d;
-		var res = []
-		data.items.forEach(_i => res.push({
-			name: _i.metadata.name,
-			namespace: _i.metadata.namespace,
-			status: _i.status.conditions[0].type
-		}));
-		return res;
-	}, _e => {
-		return _e;
-	});
+		.then(_d => {
+			var data = _d;
+			var res = []
+			data.items.forEach(_i => res.push({
+				name: _i.metadata.name,
+				namespace: _i.metadata.namespace,
+				status: _i.status.conditions[0].type
+			}));
+			return res;
+		}, _e => {
+			return _e;
+		});
 }
 
 e.getAllDeploymentsForNamespace = (_namespace) => {
 	return req.get(_baseURL + "/namespaces/" + _namespace + "/deployments")
-	.then(_d => {
-		var data = _d;
-		var res = []
-		data.items.forEach(_i => res.push({
-			name: _i.metadata.name,
-			namespace: _i.metadata.namespace,
-			status: _i.status.conditions[0].type
-		}));
-		return res;
-	}, _e => {
-		return _e;
-	});
+		.then(_d => {
+			var data = _d;
+			var res = []
+			data.items.forEach(_i => res.push({
+				name: _i.metadata.name,
+				namespace: _i.metadata.namespace,
+				status: _i.status.conditions[0].type
+			}));
+			return res;
+		}, _e => {
+			return _e;
+		});
 }
 
 e.getDeployment = (_namespace, _name) => {
 	return req.get(_baseURL + "/namespaces/" + _namespace + "/deployments/" + _name)
-	.then(_d => {
-		return _d;
-	}, _e => {
-		return _e;
-	});
+		.then(_d => {
+			return _d;
+		}, _e => {
+			return _e;
+		});
 }
 
 e.createDeployment = (_namespace, _name, _image, _port, _envVars) => {
@@ -57,10 +57,10 @@ e.createDeployment = (_namespace, _name, _image, _port, _envVars) => {
 		"spec": {
 			"replicas": 1,
 			"selector": {
-		    	"matchLabels": {
-		    		"app": _name
-		    	}
-		    },
+				"matchLabels": {
+					"app": _name
+				}
+			},
 			"template": {
 				"metadata": {
 					"labels": {
@@ -84,53 +84,58 @@ e.createDeployment = (_namespace, _name, _image, _port, _envVars) => {
 			}
 		}
 	};
+
+	if (process.env.DOCKER_SECRET) {
+		let secretList = process.env.DOCKER_SECRET.split(",");
+		data.spec.template.spec.imagePullSecrets = secretList.map(_s => { return { name: _s } });
+	}
 	return req.post(_baseURL + "/namespaces/" + _namespace + "/deployments", data)
-	.then(_d => {
-		return data;
-	}, _e => {
-		return _e;
-	});
+		.then(_d => {
+			return data;
+		}, _e => {
+			return _e;
+		});
 }
 
 e.updateDeployment = (_namespace, _name, _image, _port, _envVars) => {
 	console.log("Updating the deployment :: ", _namespace, _name, _image);
 	var data = {
-  		"spec": {
-		    "template": {
-		      "spec": {
-		        "containers": [
-		          {
-		            "image": _image,
-		            "name": _name,
-		            "ports": [
+		"spec": {
+			"template": {
+				"spec": {
+					"containers": [
 						{
-							"containerPort": _port
+							"image": _image,
+							"name": _name,
+							"ports": [
+								{
+									"containerPort": _port
+								}
+							],
+							"env": _envVars
 						}
-					],
-		            "env": _envVars
-		          }
-		        ]
-		      }
-		    }
-	  	}
+					]
+				}
+			}
+		}
 	};
 	return req.patch(_baseURL + "/namespaces/" + _namespace + "/deployments/" + _name, data)
-	.then(_d => {
-		return data;
-	}, _e => {
-		return _e;
-	});
+		.then(_d => {
+			return data;
+		}, _e => {
+			return _e;
+		});
 }
 
 e.deleteDeployment = (_namespace, _name) => {
 	console.log("Deleting the deployment :: ", _namespace, _name);
 	var data = {};
 	return req.delete(_baseURL + "/namespaces/" + _namespace + "/deployments/" + _name, data)
-	.then(_d => {
-		return data;
-	}, _e => {
-		return _e;
-	});
+		.then(_d => {
+			return data;
+		}, _e => {
+			return _e;
+		});
 }
 
 module.exports = e;
