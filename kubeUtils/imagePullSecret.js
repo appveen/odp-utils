d3a17453e9a0f81bd6ea69f05247f87df0f60dbf
Shobhit Agarwal <shobhit@capiot.com>
2018-07-24 18:38:30 +0530

    imagePullSecret using API test1

--- kubeUtils/imagePullSecret.js
+++ kubeUtils/imagePullSecret.js
@@ -1,30 +1,41 @@
-let exec = require('child_process').exec;
 let e = {};
 
-function execCommand(command, errMsg) {
-    return new Promise((resolve, reject) => {
-        exec(command, (_err, _stdout) => {
-            if (_err) {
-                logger.error(`ERROR :: ${command}`);
-                logger.error(_err);
-                return reject(new Error(errMsg));
-            }
-            logger.info(`SUCCESS :: ${command}`);
-            logger.debug(_stdout);
-            return resolve(_stdout);
-        });
-    })
+const req = require("./requestHandler");
+
+const _baseURL = "/apis/apps/v1";
+var fs = require('fs');
+
+function base64_encode(file) {
+    var bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
 }
 
-e.createSecret = function (ns) {
-    if (process.env.DOCKER_USER && process.env.DOCKER_PASSWORD && process.env.DOCKER_HOST && process.env.DOCKER_EMAIL) {
-        logger.info("Creating secret");
-        let command = `kubectl create secret docker-registry regsecret --docker-server=${process.env.DOCKER_HOST} --docker-username=${process.env.DOCKER_USER} --docker-password=${process.env.DOCKER_PASSWORD} --docker-email=${process.env.DOCKER_EMAIL} --namespace ${ns}`
-        return execCommand(command, "Could not create docker image pull secret");
-    } else {
-        logger.info("Information to create secret not found");
-        return Promise.resolve();
-    }
+e.createSecret = (ns) => {
+    console.log("Creating a new secret :: in ", ns);
+    let file = process.env.DOCKER_CONFIG;
+    logger.info("++++++++++"+file);
+    let base64En = base64_encode(file);
+    logger.info(base64En);
+    var data = {
+        "kind": "Secret",
+        "apiVersion": "v1",
+        "metadata": {
+            "name": "regsecret",
+            "namespace": ns
+        },
+        "data": {
+            ".dockerconfigjson": base64En
+        },
+        "type": "kubernetes.io/dockerconfigjson"
+    };
+    logger.info(JSON.stringify(data));
+    return req.post(_baseURL + "/namespaces/" + ns + "/secrets", data)
+        .then(_d => {
+            return data;
+        }, _e => {
+            return _e;
+        });
 }
+
 
 module.exports = e;
