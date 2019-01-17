24d1ce4ede2ad94902fba46f0c60b420e3e3b495
Mr7Sir <yash.v@capiotl.com>
2019-01-17 16:54:19 +0530

    converted request-promise to request

--- kubeUtils/requestHandler.js
+++ kubeUtils/requestHandler.js
@@ -2,7 +2,7 @@
 
 var e = {};
 
-const req = require('request-promise');
+const req = require('request');
 const fs = require("fs");
 
 const HOST = process.env.KUBERNETES_SERVICE_HOST;
@@ -12,81 +12,120 @@ const URL = "https://" + HOST + ":" + PORT;
 const TOKEN = process.env.ODP_TOKEN;
 
 var odp_token = "";
-if(TOKEN) odp_token = fs.readFileSync(TOKEN);
+if (TOKEN) odp_token = fs.readFileSync(TOKEN);
 
 e.get = (_api) => {
 	var options = {
 		method: "GET",
-		uri: URL + _api,
-		strictSSL: false,
+		url: URL + _api,
 		json: true,
-		headers:{
+		headers: {
+			'Content-Type': 'application/json',
 			"Authorization": "Bearer " + odp_token
 		},
-		resolveWithFullResponse: true
 	}
-	return req(options)
+	return new Promise((resolve, reject) => {
+		req(options, function (err, res, body) {
+			if (err) {
+				return reject(err);
+			}
+			else {
+				return resolve({ statusCode: res.statusCode, body: body });
+			}
+		})
+	})
 }
 
 e.post = (_api, _body) => {
 	var options = {
 		method: "POST",
-		uri: URL + _api,
-		strictSSL: false,
-		headers:{
+		url: URL + _api,
+		headers: {
+			'Content-Type': 'application/json',
 			"Authorization": "Bearer " + odp_token
 		},
-	    json: true,
-	    body: _body,
-		resolveWithFullResponse: true
+		json: true,
+		body: _body,
 	}
-	return req(options)
+	return new Promise((resolve, reject) => {
+		req(options, function (err, res, body) {
+			if (err) {
+				return reject(err);
+			}
+			else {
+				return resolve({ statusCode: res.statusCode, body: body });
+			}
+		})
+	})
 }
 
 e.patch = (_api, _body) => {
 	var options = {
 		method: "PATCH",
-		uri: URL + _api,
-		strictSSL: false,
-		headers:{
+		url: URL + _api,
+		headers: {
 			"Authorization": "Bearer " + odp_token,
-	    	"Content-Type": "application/merge-patch+json"
+			"Content-Type": "application/merge-patch+json"
 		},
-	    json: true,
-	    body: _body,
-		resolveWithFullResponse: true
+		json: true,
+		body: _body
 	}
-	return req(options)
+	return new Promise((resolve, reject) => {
+		req(options, function (err, res, body) {
+			if (err) {
+				return reject(err);
+			}
+			else {
+				return resolve({ statusCode: res.statusCode, body: body });
+			}
+		})
+	})
 }
 
 e.delete = (_api, _body) => {
 	var options = {
 		method: "DELETE",
-		uri: URL + _api,
-		strictSSL: false,
-		headers:{
+		url: URL + _api,
+		headers: {
+			'Content-Type': 'application/json',
 			"Authorization": "Bearer " + odp_token
 		},
-	    json: true,
-	    body: _body,
-		resolveWithFullResponse: true
+		json: true,
+		body: _body
 	}
-	return req(options)
+	return new Promise((resolve, reject) => {
+		req(options, function (err, res, body) {
+			if (err) {
+				return reject(err);
+			}
+			else {
+				return resolve({ statusCode: res.statusCode, body: body });
+			}
+		})
+	})
 }
 
 e.put = (_api, _body) => {
 	var options = {
 		method: "PUT",
-		uri: URL + _api,
-		strictSSL: false,
-		headers:{
+		url: URL + _api,
+		headers: {
+			'Content-Type': 'application/json',
 			"Authorization": "Bearer " + odp_token
 		},
-	    json: true,
-	    body: _body,
-		resolveWithFullResponse: true
+		json: true,
+		body: _body
 	}
-	return req(options)
+	return new Promise((resolve, reject) => {
+		req(options, function (err, res, body) {
+			if (err) {
+				return reject(err);
+			}
+			else {
+				return resolve({ statusCode: res.statusCode, body: body });
+			}
+		})
+	})
 }
 
 module.exports = e;
