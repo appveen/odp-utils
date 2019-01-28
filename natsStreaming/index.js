16e9251519f2823d1d1497d406cfabab896bf3fb
Piyush Verma <piyush@appveen.com>
2019-01-28 18:05:35 +0530

    cleanup

--- natsStreaming/index.js
+++ natsStreaming/index.js
@@ -1,36 +1,36 @@
-let e = {};
 var NATS = require('node-nats-streaming');
 var client = null;
-var init= (clientId,clusterName,config) =>{
+var log4js = require('log4js');
+var logger = log4js.getLogger('odp-utils-nats-streaming');
+var init = (clusterName, clientId, config) => {
 
-    client = NATS.connect(clusterName,clientId , config);
-    const logger = global.logger;
-    client.on('error', function(err) {
+    client = NATS.connect(clusterName, clientId, config);
+    client.on('error', function (err) {
         logger.error(err.message);
     });
-     
-    client.on('connect', function() {
+
+    client.on('connect', function () {
         logger.info('NATS connected');
     });
-     
-    client.on('disconnect', function() {
+
+    client.on('disconnect', function () {
         logger.info('NATS disconnect');
     });
-     
-    client.on('reconnecting', function() {
+
+    client.on('reconnecting', function () {
         logger.info('NATS reconnecting');
     });
-     
-    client.on('reconnect', function() {
+
+    client.on('reconnect', function () {
         logger.info('NATS reconnect');
     });
-     
-    client.on('close', function() {
+
+    client.on('close', function () {
         logger.info('NATS close');
     });
     return client;
 }
 
 module.exports = {
- init:init
+    init: init
 };
