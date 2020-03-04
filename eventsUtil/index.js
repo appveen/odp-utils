6e362e36e8efdc02bc6a5fa1b54971a8a7835080
Jugnu <jugnu@appveen.com>
2020-03-04 15:53:49 +0530

    added partner in method

--- eventsUtil/index.js
+++ eventsUtil/index.js
@@ -6,25 +6,41 @@ function setNatsClient(natsClient) {
     client = natsClient;
 }
 
-function publishEvent(eventId, source, req, doc) {
-    let logger = global.logger;
-    let payload = {
-        "eventId" : eventId,
-        "source": source,
-        "documentId" : doc._id,
-        "documentName" : doc.name,
-        "app": doc.app,
-        "triggerType" : "user",
-        "triggerId" : req.get('user'),
-        "timestamp": new Date().toISOString(),
-        "priority" : eventPriorityMap[eventId],
-        "txnId" : req.get('txnId')
-    }
-    if(client) {
-        client.publish('events', JSON.stringify(payload));
-        logger.info('Event published');
-    } else {
-        logger.error('client not found to publish events');
+/**
+ * 
+ * @param {string} eventId The Event ID to Identify an Event
+ * @param {string} source Module from which it is originated
+ * @param {*} req The Incomming Request Object
+ * @param {*} doc The Document Object of the Source Module
+ * @param {*} [partner] The Partner Document Object. (Only if origin is flow)
+ */
+function publishEvent(eventId, source, req, doc, partner) {
+    try {
+        let logger = global.logger;
+        let payload = {
+            "eventId": eventId,
+            "source": source,
+            "documentId": doc._id,
+            "documentName": doc.name,
+            "app": doc.app,
+            "triggerType": "user",
+            "triggerId": req.get('user'),
+            "timestamp": new Date().toISOString(),
+            "priority": eventPriorityMap[eventId],
+            "txnId": req.get('txnId')
+        }
+        if (partner) {
+            payload.partnerId = partner._id;
+            payload.partnerName = partner.name;
+        }
+        if (client) {
+            client.publish('events', JSON.stringify(payload));
+            logger.info('Event published');
+        } else {
+            logger.error('client not found to publish events');
+        }
+    } catch (e) {
+        logger.error('publishEvent', e);
     }
 }
 
